f2ace971ed801aa501a69764e2c1e5ec2c2afa2b
ducmanh0418 <ducmanh0418@gmail.com>
2017-12-28 16:19:12 +0700

    login
    login
    
    update-users

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,4 +3,12 @@ class StaticPagesController < ApplicationController
     @products = Product.info.order(:name).page(params[:page]).
       per Settings.product_per_page
   end
+
+  def create
+    if !session[:cart]
+      @cart = Cart.new user_id: current_user.id
+      @cart.save
+      session[:cart]=@cart.id
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'products/show'
+
   root "static_pages#home"
   get "/signup", to: "users#new"
   get "/login", to: "login_sessions#new"
   post "/login", to: "login_sessions#create"
   delete "logout", to: "login_sessions#destroy"
   resources :users
+  resources :products
 end
